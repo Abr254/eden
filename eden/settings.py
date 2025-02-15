866fcdfb2bad2a710a78bd2fc76a92824d30c053
* <kemboiabu22@gmail.com>
2025-02-15 18:19:12 +0300

    settings.py

--- eden/settings.py
+++ eden/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-guo279(wb%&21or3!(+*14-7$om29t1!8uwg1xa-nbsb8!cj1q
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['eden-p7ah.onrender.com]
+ALLOWED_HOSTS = ['eden-3uly.onrender.com]
 
 
 # Application definition
